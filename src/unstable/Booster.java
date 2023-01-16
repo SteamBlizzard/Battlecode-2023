77920d5860458903dedaf121c43e509dd8a4ea67
Daniel Sabourov <dan.sabo@live.ca>
2023-01-16 15:23:13 -0700

    We unstable out here

---
+++ src/unstable/Booster.java
@@ -0,0 +1,10 @@
+package unstable;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class Booster extends Robot {
+    public Booster(RobotController rc) throws GameActionException {
+        super(rc);
+    }
+}
